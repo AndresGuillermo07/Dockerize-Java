9fe9df6fc603a1c92ee5c69ef571b60eb494230c
Andres_Aviles <andresaviles0721@gmail.com>
2024-11-29 17:48:26 -0500

    Modifies

--- Spring-Docker-Finish/src/main/java/com/example/repository/IUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.User;
-import org.springframework.data.repository.ListCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IUserRepository extends ListCrudRepository<User, Long> {
-}
